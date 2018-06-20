193bc1ff6e1e45ad158048761baff058aa2b9b5c
GleasonBian <bianliuzhu@gmail.com>
2018-06-20 21:07:07 +0800

    parent.vue文件中定义传递给子组件数据,使用v-bind绑定子组件上

--- src/components/Parent-childComponentsCommunication/Parent.vue
+++ src/components/Parent-childComponentsCommunication/Parent.vue
@@ -1,7 +1,14 @@
 <template>
 <div>
   <h1>父组件</h1>
-  <quote-child-components></quote-child-components>
+
+  <h3>父组件传递数据给子组件</h3>
+  <ul>
+    <li>1.在 data 中定义需要传递的数据</li>
+    <li>2.在template中引用子组件的地方使用v-bind(简写 ':')绑定数据</li>
+    <li>3.切换到 到 子组件(Child.vue)中使用props接收来自父组件的数据</li>
+  </ul>
+  <quote-child-components :transmit-to-child-components=msg></quote-child-components>
 </div>
 </template>
 <script>
@@ -9,7 +16,9 @@
   import Child from './Child'
 export default {
     data() {
-      return {}
+      return {
+        msg:[1,2,3]
+      }
     },
     methods:{
 
