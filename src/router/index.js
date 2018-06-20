e75836c638ba6df34b28bd893f129276e4a2949d
GleasonBian <bianliuzhu@gmail.com>
2018-06-20 20:00:04 +0800

    router/index.js添加注释

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+//1.引入 组件
 import Child from '@/components/VueRouter/child/child'
 import Parent from '@/components/VueRouter/parent/parent'
+//2.使用 Router
 Vue.use(Router)
-
+//3.在 下面配置路由
 export default new Router({
   routes: [
     {
