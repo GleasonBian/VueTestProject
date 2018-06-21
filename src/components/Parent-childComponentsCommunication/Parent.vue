b4e30d1b55c38e0b29ea1a70519a0539952067be
Wilson <bianliuzhu@gmail.com>
2018-06-21 15:18:02 +0800

    增加说明

--- src/components/Parent-childComponentsCommunication/Parent.vue
+++ src/components/Parent-childComponentsCommunication/Parent.vue
@@ -1,16 +1,22 @@
 <template>
-<div>
-  <h1>父组件</h1>
+  <div>
+    <h1>父组件</h1>
 
-  <h3>父组件传递数据给子组件</h3>
-  <ul>
-    <li>1.在 data 中定义需要传递的数据</li>
-    <li>2.在template中引用子组件的地方使用v-bind(简写 ':')绑定数据</li>
-    <li>3.切换到 到 子组件(Child.vue)中使用props接收来自父组件的数据</li>
-  </ul>
-  <!-- 注意: to-child-data 必须短横线命名 在 child.vue 文件 props 属性中 使用 toChildData 驼峰命名 名字必须一致 否则子组件接收不到数据 -->
-  <quote-child-components :to-child-data="msg"></quote-child-components>
-</div>
+    <h3>父组件传递数据给子组件</h3>
+    <ul>
+      <li>1.在 data 中定义需要传递的数据</li>
+      <li>2.在template中引用子组件的地方使用v-bind(简写 ':')绑定数据</li>
+      <li>3.切换到 到 子组件(Child.vue)中使用props接收来自父组件的数据</li>
+    </ul>
+    <!--
+      注意:
+        1.使用 v-bind 绑定 要传递给子组件的数据
+        2.to-child-data : 给子组件的数据名字,必须短横线命名
+        3.在 child.vue 文件 props 属性中 接收来自父组件的数据
+        4.使用 toChildData 驼峰命名, 名字必须与父组件传递数名称一致 否则子组件接收不到数据
+    -->
+    <quote-child-components :to-child-data="msg"></quote-child-components>
+  </div>
 </template>
 <script>
 //1.引入子组件
@@ -32,5 +38,3 @@ export default {
 </script>
 <style>
 </style>
-
-
