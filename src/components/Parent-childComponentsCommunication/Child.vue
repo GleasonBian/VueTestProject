f8676db84203131e0719d6359bf75a6aab661cb3
Wilson <bianliuzhu@gmail.com>
2018-06-21 17:07:33 +0800

    子组件触发父组件方法

--- src/components/Parent-childComponentsCommunication/Child.vue
+++ src/components/Parent-childComponentsCommunication/Child.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
-    <h1>子组件</h1>
+    <h1>子组件<span>({{fromMsg}})</span></h1>
     <ul v-for="(item,index) in toChildData">
       <li>{{index}}:{{item}}</li>
     </ul>
+    <div v-if="!(id==1)">
+      <h2>触发父组件方法</h2>
+      <!-- 三.1:定义触父组件的方法 "changeParentMethod -->
+      <button @click="changeParentMethod">修改父组件数据</button>
+    </div>
   </div>
 </template>
 <script>
@@ -12,11 +17,23 @@ export default {
     return {};
   },
   props: {
-    toChildData: Array
+    toChildData: Array,
+    fromMsg: {
+      type: String,
+      default: ""
+    },
+    id: Number
   },
   methods: {
     getMsg() {
-      console.log(this.toChildData);
+      //console.log(this.toChildData);
+    },
+    changeParentMethod() {
+      // 三.2:通过 $emit 调用父组件方法
+      // 三.3:第一个参数 在父组件 页面应用子组件的地方 定义的 供子组件触发方法的名称 responseChild
+      // 三.4:第二参数 带给父组件的数据
+      // 三.5:转到父组件
+      this.$emit("responseChild", "收到来自子组件的消息");
     }
   },
   created() {
--- src/components/Parent-childComponentsCommunication/Parent.vue
+++ src/components/Parent-childComponentsCommunication/Parent.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <h1>父组件</h1>
-
+    <h3>一.引用组件</h3>
+    <ul>
+      <li>一.1:使用 import 引入需要的组件</li>
+      <li>一.2. 注册组件</li>
+      <li>一.2.1: quote-child-components 是在template中使用的名子,不能使用驼峰,html不认识大小写</li>
+      <li>一.2.2: Child 是 import 组件的名字</li>
+    </ul>
     <h3>二.父组件传递数据给子组件</h3>
     <ul>
       <li>二.1.在 data 中定义需要传递的数据</li>
@@ -15,7 +21,21 @@
         二.4.3:在 child.vue 文件 props 属性中 接收来自父组件的数据
         二.4.4:使用 toChildData 驼峰命名, 名字必须与父组件传递数名称一致 否则子组件接收不到数据
     -->
-    <quote-child-components :to-child-data="msg"></quote-child-components>
+    <quote-child-components :to-child-data="msg" :from-msg="toChild" :id="idOne"></quote-child-components>
+    <h3>三.子组件与父组件通信</h3>
+    <ul>
+      <li>
+        <h4>说明:</h4>
+        <p>在vue中不允许子组件改变父组件数据，因为vue是单向数据传递，可以通过触发事件来通知父组件改变数据，从而达到改变子组件数据的目的.</p>
+      </li>
+      <li>三.1:在子组件内</li>
+      <li>三.6:在父组件页面引入子组件</li>
+      <li>三.7:在父组件内定义一个供子组件触发的方法: ChengeParent</li>
+      <li>三.8:在父组件引用子组件的地方 使用 v-con(简写:@) 绑定 在子组件$emit内定义的方法名 responseChild 并调用父组件内的方法 : @responseChild="ChengeParent" </li>
+      <li>父组件数据被子组件修改:<span style="font-size:20px;color:red;font-weight:800;">{{ParentData}}</span></li>
+    </ul>
+
+    <quote-child-components :from-msg="toChild" @responseChild="ChengeParent" :id="idTwo"></quote-child-components>
   </div>
 </template>
 <script>
@@ -25,10 +45,19 @@ import Child from "./Child";
 export default {
   data() {
     return {
-      msg: [1, 2, 3]
+      msg: [1, 2, 3],
+      toChild: "我是被引用的子组件",
+      ParentData: 1,
+      idOne: 1,
+      idTwo: 2
     };
   },
-  methods: {},
+  methods: {
+    ChengeParent(event) {
+      this.ParentData++;
+      alert(event);
+    }
+  },
   //一.2. 注册组件
   components: {
     // 一.2.1: quote-child-components 是在template中使用的名子,不能使用驼峰,html不认识大小写
@@ -37,5 +66,6 @@ export default {
   }
 };
 </script>
-<style>
+<style scoped>
+
 </style>
