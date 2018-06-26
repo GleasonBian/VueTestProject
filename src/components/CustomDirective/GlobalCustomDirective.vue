d4e478083ea02cd554d6ff61bb705e3fdaf6febd
GleasonBian <bianliuzhu@gmail.com>
2018-06-27 00:14:26 +0800

    全局自定义指令

--- src/components/CustomDirective/GlobalCustomDirective.vue
+++ src/components/CustomDirective/GlobalCustomDirective.vue
@@ -3,10 +3,23 @@
     <h3>全局自定义指令</h3>
     <p>作用:对普通 DOM 元素进行底层操作</p>
     <div v-lang="color">{{num}}</div>
-    <!---->
     <p>
       <button @click="add">add</button>
     </p>
+    <h3>实现全局自定义指令步骤</h3>
+    <ul>
+      <li>1.在 main.js 中
+        <pre>
+          //创建 Vue 全局指令
+          Vue.directive('lang', function (el, binding) {
+            console.log(el) //输出 绑定全局自定指令的 元素
+            console.log(binding) //输出的是一个对象obj
+            el.style = 'color:' + binding.value //binding.value就是color颜色red。
+          })
+        </pre>
+        注意: 在 Vue 实例 前创建自定义指令
+      </li>
+    </ul>
   </div>
 
 </template>
@@ -23,6 +36,9 @@
       add: function () {
         this.num++;
       }
+    },
+    created(){
+      this.add();
     }
   };
 </script>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,14 @@ Vue.use(VueRouter);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+
+//创建 Vue 全局指令
+Vue.directive('lang', function (el, binding) {
+  console.log(el) //标签<div v-lang="color">{{num}}</div>
+  console.log(binding) //输出的是一个对象obj
+  el.style = 'color:' + binding.value //binding.value就是color颜色red。
+})
+
 new Vue({
   el: '#app',
   data() {
@@ -22,8 +30,3 @@ new Vue({
   template: '<App/>',
 
 }).$mount('#app'); // 手动挂载，#app
-Vue.directive('lang', function (el, binding) {
-  console.log(el) //标签<div v-lang="color">{{num}}</div>
-  console.log(binding) //输出的是一个对象obj
-  el.style = 'color:' + binding.value //binding.value就是color颜色red。
-})
