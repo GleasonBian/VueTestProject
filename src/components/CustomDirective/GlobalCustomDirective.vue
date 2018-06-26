04326654428461132e0122475650f7dfc8f091ff
Wilson <bianliuzhu@gmail.com>
2018-06-26 12:19:35 +0800

    创建自定义指令全局与局部组件

---
+++ src/components/CustomDirective/GlobalCustomDirective.vue
@@ -0,0 +1,18 @@
+<template>
+<h3>自定义指令</h3>
+<p>作用:对普通 DOM 元素进行底层操作</p>
+<ul>
+  实现步骤:
+  <li></li>
+</ul>
+</template>
+<script>
+export default {
+
+
+}
+</script>
+<style scoped>
+
+
+</style>
---
+++ src/components/CustomDirective/LocalCustomDirective.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <h3>局部自定义指令</h3>
+  
+</div>
+<h3></h3>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+
+</style>
